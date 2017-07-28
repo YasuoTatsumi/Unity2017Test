14ca006cae505d8ab752cf368da7b7759d2e5661
YasuoTatsumi <pheonixbaster@yahoo.co.jp>
2017-07-28 17:57:36 +0900

    SpriteAtlas作成してテスト

---
+++ Assets/Resources/SpriteAtlas/Readme.txt
@@ -0,0 +1,7 @@
+﻿参考URL
+http://kan-kikuchi.hatenablog.com/entry/SpriteAtlas
+
+注意点
+1.パックしたい画像はあらかじめTextureTypeをSpriteにする必要がある。
+2.Resources内にSpriteAtlasと元の画像の両方を入れてしまうと元の画像も入るようなので分ける必要がある。
+
---
+++ Assets/Scripts/SpriteTestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteTestScript : MonoBehaviour {
+    [SerializeField]
+    Image image;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log( image.sprite.texture.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
